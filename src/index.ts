b1e6761bc1782d1fdf14201450a5eebeaedb7e9f
Yan Zhang <yanzh@microsoft.com>
2018-09-04 16:10:51 +0800

    Export APIs.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { TelemetryWrapper } from "./TelemetryWrapper";
 export { Session } from "./Session";
+// For next
+export { sendError, sendOpEnd, sendOpStart, getWrappedCommand, initilizeFromJsonFile, initilizeFromAttributes } from "./next/main";
